8a497ce8ba089832526e8505ff1acafb324279d5
marthinusswart <marthinus.swart@gmail.com>
2025-06-30 22:10:51 +1000

    Add the service

---
+++ src/main/java/com/mattswart/realtimeweatherservice/service/RealTimeWeatherService.java
@@ -0,0 +1,14 @@
+package com.mattswart.realtimeweatherservice.service;
+
+public class RealTimeWeatherService {
+    // This class will contain methods to interact with the OpenWeatherMap API
+    // and provide real-time weather data.
+
+    // Example method to get current weather by city name
+    public String getCurrentWeatherByCity(String cityName) {
+        // Implementation will go here
+        return String.format("Current weather data for %s",  cityName);
+    }
+
+    // Additional methods for other functionalities can be added here
+}
